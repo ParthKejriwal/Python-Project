c80d62008a5ea7d19511bba6ec2951d11f7402fc
ParthKejriwal <62254837+ParthKejriwal@users.noreply.github.com>
2020-10-04 11:18:42 +0530

    This is a game created by me
    A number guessing game

--- guessingGame.py
+++ guessingGame.py
@@ -1,20 +1,20 @@
 import random
- 
-print("Welcome to the number guessing game, try to guess the number - between 1 and 100")
- 
-rand_num = random.randint(1, 100)
-count = 0
- 
-def main():
-    guess = int(input("Guess: "))
- 
-    if guess == rand_num:
-        print("Correct")
-    if guess > rand_num:
-        print("Guess Lower")
-        main()
-    if guess < rand_num:
-        print("Guess Higher")
-        main()
- 
-main()
+number = random.randint(1, 99)
+
+playerName = input("Hello, What's your name?")
+numberOfGuesses = 0
+print('Okay! '+ playerName+ ' I am Thinking of a number between 1 and 99.Please try to guess it:')
+
+while numberOfGuesses < 5:
+    guess = int(input())
+    numberOfGuesses += 1
+    if guess < number:
+        print('Your guess is too low')
+    if guess > number:
+        print('Your guess is too high')
+    if guess == number:
+        break
+if guess == number:
+    print('You guessed the number in ' + str(numberOfGuesses) + ' tries!')
+else:
+    print('You did not guess the number, The number was ' + str(number))
